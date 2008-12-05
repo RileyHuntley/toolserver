d87544bdaefe97ef6c284fac7a68cc4a3e48ef62
emijrp <emijrp@c6dfa21e-ad1b-11dd-a35d-6bd55b4a219c>
2008-12-05 21:36:41 +0000

    ranking de ediciones en 30 idiomas

--- tarea008.py
+++ tarea008.py
@@ -3,10 +3,27 @@
 import datetime
 import os, re, wikipedia
 
-tras1={u"es": u"Ranking de ediciones", u"fr": u"Utilisateurs par nombre d'éditions", u"pl": u"Użytkownicy według liczby edycji"}
-tras2={u"es": u"Ranking de ediciones (incluye bots)", u"fr": u"Utilisateurs par nombre d'éditions (bots inclus)", u"pl": u"Użytkownicy według liczby edycji (w tym boty)"}
+begin=u"''Please, translate this into your language and delete the english text'': This table shows '''first {{{1}}} users with more edits''' in this Wikipedia. Bots are not included.\n\n''If you want to change page title, contact to [[:es:User talk:Emijrp]]. Thanks.''\n\n<center>\n{| class='wikitable sortable' style='text-align:center;'\n! #\n! User\n! Edits\n"
+begin2=u"''Please, translate this into your language and delete the english text'': This table shows '''first {{{1}}} users with more edits''' in this Wikipedia. Bots are included.\n\n''If you want to change page title, contact to [[:es:User talk:Emijrp]]. Thanks.''\n\n<center>\n{| class='wikitable sortable' style='text-align:center;'\n! #\n! User\n! Edits\n"
+end=u"|}\n</center>\n\n''Please, put here a category similar to <nowiki>[[Category:Wikipedia statistics]]</nowiki>.''\n\n[[cs:Wikipedie:Nejaktivnější wikipedisté]]\n[[de:Wikipedia:Beitragszahlen]]\n[[es:Wikipedia:Lista de wikipedistas por número de ediciones]]\n[[fr:Wikipédia:Liste des Wikipédiens par nombre d'éditions]]\n[[zh-classical:維基大典:編輯次數]]\n[[id:Wikipedia:Daftar pengguna menurut jumlah suntingan]]\n[[is:Wikipedia:Notendur eftir breytingafjölda]]\n[[he:ויקיפדיה:נתונים סטטיסטיים/משתמשים/1-100]]\n[[jv:Wikipedia:Daftar panganggo miturut cacahé suntingan]]\n[[ka:ვიკიპედია:ვიკიპედიელები აქტიურობის მიხედვით]]\n[[lt:Vikipedija:Naudotojai pagal keitimų skaičių]]\n[[hu:Wikipédia:Szerkesztők listája szerkesztésszám szerint]]\n[[ja:Wikipedia:編集回数の多いウィキペディアンの一覧]]\n[[pl:Wikipedia:Najaktywniejsi wikipedyści]]\n[[pt:Wikipedia:Lista de wikipedistas por número de edições]]\n[[sq:Wikipedia:Lista e Wikipedianëve sipas redaktimeve]]\n[[uk:Вікіпедія:Найактивніші]]\n[[bat-smg:Vikipedėjė:Nauduotuojē palē keitėmu skaitliu]]\n[[zh:Wikipedia:最多贡献的用户]]"
+end2=end
 
-for lang in ['es']:
+tras1={
+u"eo": u"Listo de uzantoj laŭ redaktonombro",
+u"es": u"Ranking de ediciones", 
+u"fr": u"Utilisateurs par nombre d'éditions", 
+u"pl": u"Użytkownicy według liczby edycji",
+u"sv": u"Lista över Wikipedia-användare sorterad efter antalet redigeringar",
+}
+tras2={
+u"eo": u"Listo de uzantoj laŭ redaktonombro (inkluzivante robotojn)",
+u"es": u"Ranking de ediciones (incluye bots)", 
+u"fr": u"Utilisateurs par nombre d'éditions (bots inclus)", 
+u"pl": u"Użytkownicy według liczby edycji (w tym boty)",
+}
+
+lll=['es', 'eo', 'hu', 'ca', 'tr', 'ro', 'vo', 'fi', 'it', 'nl', 'ru', 'sv', 'no', 'da', 'ar', 'ko', 'sr', 'sl', 'vi', 'bg', 'et', 'ht', 'fa', 'hr', 'new', 'nn', 'te', 'gl', 'th', 'simple']
+for lang in lll:
 	site=wikipedia.Site(lang, 'wikipedia')
 	
 	bots=[u'BOTpolicia', u'AVBOT', u'CommonsDelinker', u'Eskimbot']
@@ -36,7 +53,7 @@ for lang in ['es']:
 			wikipedianm+=name
 	
 	
-	os.system('mysql -h sql-s3 -e "use %swiki_p;select user_name, user_editcount from user order by user_editcount desc limit 5000;" > /home/emijrp/temporal/tarea008.data' % lang)
+	os.system('mysql -h %swiki-p.db.toolserver.org -e "use %swiki_p;select user_name, user_editcount from user where user_editcount!=0 order by user_editcount desc limit 5000;" > /home/emijrp/temporal/tarea008.data' % (lang, lang))
 	
 	f=open('/home/emijrp/temporal/tarea008.data', 'r')
 	sql=unicode(f.read(), 'utf-8')
@@ -78,20 +95,48 @@ for lang in ['es']:
 	planti2+=u"|USUARIO DESCONOCIDO\n}}<noinclude>{{uso de plantilla}}</noinclude>"
 	planti+=u"|-\n| colspan=3 | Véase también [[Wikipedia:Ranking de ediciones]]<br/>Actualizado a las {{subst:CURRENTTIME}} (UTC) del  {{subst:CURRENTDAY}}/{{subst:CURRENTMONTH}}/{{subst:CURRENTYEAR}} por [[Usuario:Toolserver|Toolserver]] \n|}<noinclude>{{uso de plantilla}}</noinclude>"
 	
+	resume=u"BOT - Updating ranking"
+	title=u''
+	#first ranking
 	if tras1.has_key(lang):
-		page=wikipedia.Page(site, u"%s:%s" % (wikipedianm, tras1[lang]))
+		title=u"%s:%s" % (wikipedianm, tras1[lang])
 	else:
-		page=wikipedia.Page(site, u"%s:Users by number of edits" % wikipedianm)
-	page.put(s, "BOT - Updating ranking")
+		title=u"%s:List of Wikipedians by number of edits" % wikipedianm
+	page=wikipedia.Page(site, title)
+	page.put(s, resume)
+	#begin & end
+	page=wikipedia.Page(site, u"%s/begin" % title)
+	if not page.exists():
+		page.put(begin, resume)
+	page=wikipedia.Page(site, u"%s/end" % title)
+	if not page.exists():
+		page.put(end, resume)
 	
+	#second ranking
+	title=u''
 	if tras2.has_key(lang):
-		page=wikipedia.Page(site, u"%s:%s" % (wikipedianm, tras2[lang]))
+		title=u"%s:%s" % (wikipedianm, tras2[lang])
 	else:
-		page=wikipedia.Page(site, u"%s:Users by number of edits (bots included)" % wikipedianm)
-	page.put(sbots, "BOT - Updating ranking")
+		title=u"%s:List of Wikipedians by number of edits (bots included)" % wikipedianm
+	page=wikipedia.Page(site, title)
+	page.put(sbots, resume)
+	#begin & end
+	page=wikipedia.Page(site, u"%s/begin" % title)
+	if not page.exists():
+		page.put(begin2, resume)
+	page=wikipedia.Page(site, u"%s/end" % title)
+	if not page.exists():
+		page.put(end2, resume)
 	
-	page=wikipedia.Page(site, u"Template:RankingEdiciones")
-	page.put(planti, "BOT - Updating template")
+	#page=wikipedia.Page(site, u"Template:RankingEdiciones")
+	#page.put(planti, resume)
 	
-	page=wikipedia.Page(site, u"Template:Ediciones")
-	page.put(planti2, "BOT - Updating template")
+	#page=wikipedia.Page(site, u"Template:Ediciones")
+	#page.put(planti2, resume)
+
+	toolserver=u"This user is a bot. It won't understand you. Comments to [[:es:User talk:Emijrp]]. Thanks.\n\nThis bot is executed from [[meta:Toolserver]], so, if it is necessary, block it by nick. Other users can use the same IP address."
+	page=wikipedia.Page(site, u"User:Toolserver")
+	page.put(toolserver, u"BOT")
+	page=wikipedia.Page(site, u"User talk:Toolserver")
+	page.put(toolserver, u"BOT")
+
