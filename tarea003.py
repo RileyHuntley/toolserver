1a4e9398a088368e7053fb860fbd7ec30bac7b03
emijrp <emijrp@c6dfa21e-ad1b-11dd-a35d-6bd55b4a219c>
2009-01-24 18:53:45 +0000

    optimización

--- tarea003.py
+++ tarea003.py
@@ -11,43 +11,8 @@ global cont1
 global cont2
 PageTitles=[]
  
-def f1(j):
-	global salida
-	global cont1
-	global cont2
-
-	jj=j.title()
-	if j.exists() and not j.isRedirectPage() and not j.isDisambig():
-		jt=j.get()
-		n=re.compile(u"\[\[es:([^\]]*?)\]\]").finditer(jt)
-		es=u""
-		tt=u""
-		for k in n:
-			if not es:
-				es+=u"%s" % k.group(1)
-
-		n=re.compile(u"\[\[(de|fr|pt|it):([^\]]*?)\]\]").finditer(jt)
-		iws=u""
-		for k in n:
-			iws+=u" - [[:%s:%s|%s]]" % (k.group(1), k.group(2), k.group(1))
-
-		if re.search(ur"(?i)\{\{ *(template\:)? *taxobox", jt):
-			tt+="T"
-
-		if es:
-			cont1+=1
-			salida+=u"\n# [[%s]] - [[:en:%s|en]]%s" % (es, jj, iws)
-			if tt:
-				salida+=u" (%s)" % (tt)
-		else:
-			cont2+=1
-			salida+=u"\n# [[%s]] - [[:en:%s|en]]%s" % (jj, jj, iws)
-			if tt:
-				salida+=u" (%s)" % (tt)
- 
 #'A', 'A2', 'A3', 'A4', 'A5', 'B', 'B2', 'B3', 'C', 'C2', 'C3', 'C4', 'D', 'D2', 'E', 'E2', 'F', 'F2', 'F3', 'G', 'G2', 'G3', 'H', 'H2', 'H3', 'I', 'J', 'J2', 'J3', 'J4', 'J5', 'K', 'K2', 'L', 'L2', 'M', 'M2', 'M3', 'N', 'O', 'P', 'P2', 'P3', 'Q', 'R', 'R2', 'S', 'S2', 'S3', 'T', 'T2','U', 'V', 'W', 'W2','X-Z'
- 
- 
+
 for i in ['A', 'A2', 'A3', 'A4', 'A5', 'B', 'B2', 'B3', 'C', 'C2', 'C3', 'C4', 'D', 'D2', 'E', 'E2', 'F', 'F2', 'F3', 'G', 'G2', 'G3', 'H', 'H2', 'H3', 'I', 'J', 'J2', 'J3', 'J4', 'J5', 'K', 'K2', 'L', 'L2', 'M', 'M2', 'M3', 'N', 'O', 'P', 'P2', 'P3', 'Q', 'R', 'R2', 'S', 'S2', 'S3', 'T', 'T2','U', 'V', 'W', 'W2','X-Z']:
 	PageTitles.append(u"Wikipedia:WikiProject Missing encyclopedic articles/1911 verification/%s" % i)
  
@@ -59,49 +24,74 @@ contt1=0
 contt2=0
 progreso=u"{{subst:Progreso1911/subst"
 for p in preloadingGen:
-	tt=p.title()
-	t=p.get()
-
-	m=re.compile(u"(?im)^\#.*?\[\[([^|\]]*?)\]\]").finditer(t)
-
-	pt=[]
+	wtitle=p.title()
+	wtext=p.get()
+	
+	m=re.compile(ur"(?im)^\# *(\[\[ *(Image|File) *\:[^\]]*?\]\])?[^\[]*?\[\[(?P<page>[^|\]]*?)(\||\]\])").finditer(wtext)
+	
+	articleslist=[]
 	for i in m:
-		pt.append(u"%s" % i.group(1))
-
-	pp = [wikipedia.Page(wikipedia.Site("en", "wikipedia"), PageTitle) for PageTitle in pt]
+		articleslist.append(i.group("page"))
+	
+	pp = [wikipedia.Page(wikipedia.Site("en", "wikipedia"), PageTitle) for PageTitle in articleslist]
 	g = iter(pp)
 	pg = pagegenerators.PreloadingGenerator(g, pageNumber = 250)
-
+	
 	salida=u"{{Wikiproyecto:Enciclopedia Británica 1911/Header}}\n\n"
-	cont1=0
-	cont2=0
-	pt=[]
+	cont1=cont2=0
+	articleslist=[]
 	for j in pg:
 		if j.exists():
 			if j.isRedirectPage():
-				pt.append(u"%s" % j.getRedirectTarget().title())# esta línea es la que dice que da problemas
+				articleslist.append(j.getRedirectTarget().title())
 			else:
-				f1(j)
-
-	pp = [wikipedia.Page(wikipedia.Site("en", "wikipedia"), PageTitle) for PageTitle in pt]
+				articleslist.append(j.title())
+	
+	pp = [wikipedia.Page(wikipedia.Site("en", "wikipedia"), PageTitle) for PageTitle in articleslist]
 	g = iter(pp)
 	pg = pagegenerators.PreloadingGenerator(g, pageNumber = 250)
 	for j in pg:
 		if j.exists():
-			f1(j)
-
+			jtitle=j.title()
+			if j.exists() and not j.isRedirectPage() and not j.isDisambig():
+				jtext=j.get()
+				iws=j.interwiki()
+				es=tt=u""
+				for iw in iws:
+					if not es and iw.site().lang=='es':
+						es=iw.title()
+				
+				iwstext=u""
+				for iw in iws:
+					if iw.site().lang in ['de', 'fr', 'pt', 'it']:
+						iwstext+=u" - [[:%s:%s|%s]]" % (iw.site().lang, iw.title(), iw.site().lang)
+				
+				if re.search(ur"(?i)\{\{ *(template\:)? *taxobox", jtext):
+					tt+="T"
+				
+				if es:
+					cont1+=1
+					salida+=u"\n# [[%s]] - [[:en:%s|en]]%s" % (es, jtitle, iwstext)
+					if tt:
+						salida+=u" (%s)" % (tt)
+				else:
+					cont2+=1
+					salida+=u"\n# [[%s]] - [[:en:%s|en]]%s" % (jtitle, jtitle, iwstext)
+					if tt:
+						salida+=u" (%s)" % (tt)
+	
 	contt1+=cont1
 	contt2+=cont2
-
+	
 	porcentaje=cont1*1.0/(cont1+cont2)*100
 	progreso+=u"|%.2f" % porcentaje
-
-	wikipedia.output(u"%s" % salida)
-
-	trozos=tt.split("/")
-
-	#x=wikipedia.Page(wikipedia.Site("es", "wikipedia"), u"Wikiproyecto:Enciclopedia Británica 1911/%s" % (trozos[2]))
-	#x.put(salida, u"generando lista")
+	
+	#wikipedia.output(u"%s" % salida)
+	
+	trozos=wtitle.split("/")
+	
+	x=wikipedia.Page(wikipedia.Site("es", "wikipedia"), u"Wikiproyecto:Enciclopedia Británica 1911/%s" % (trozos[2]))
+	x.put(salida, u"BOT - Generando lista")
  
 porcentaje=contt1*1.0/(contt1+contt2)*100
 progreso+=u"|%.2f" % porcentaje
