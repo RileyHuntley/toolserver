a184228ad8c7230e1156b4f87f83a201dbc4a98c
emijrp <emijrp@c6dfa21e-ad1b-11dd-a35d-6bd55b4a219c>
2010-02-20 15:35:01 +0000

    incorporo "not conclusive"

--- commonsgooglelanguages.py
+++ commonsgooglelanguages.py
@@ -6,11 +6,43 @@ import sys
 import pagegenerators
 import urllib
 import os
+import subprocess
+import time
 
 """errors: 
 http://commons.wikimedia.org/w/index.php?title=File:Backi_petrovac_map.png&diff=35687668&oldid=31253617
 
 """
+
+def checkLanguage(desc):
+	desc_=desc
+	desc_=desc_.lower()
+	desc_=re.sub(ur"[\'\"]", ur" ", desc_)
+	desc_=re.sub(ur"\[\[(?P<link>[^\]\|]+?)\]\]", ur"\g<link>", desc_)
+	desc_=re.sub(ur"\[\[(?P<link>[^\]\|]+?)\|(?P<label>[^\]\|]+?)\]\]", ur"\g<label>", desc_)
+	desc_=re.sub(ur" ", ur"%20", desc_)
+	
+	url="http://ajax.googleapis.com/ajax/services/language/detect?v=1.0&q="
+	url+=desc_.encode("utf-8")
+	os.system('curl -e http://es.wikipedia.org/wiki/User:Emijrp "%s" > a.txt 2> /dev/null' % url)
+	print "Description:", desc
+	print "Description sent to Google", desc_
+	#raw= subprocess.Popen(["curl", "-e", "http://es.wikipedia.org/wiki/User:Emijrp", "'%s'" % url], stdout=subprocess.PIPE).communicate[0]
+	f=open("a.txt", "r")
+	raw=f.read()
+	f.close()
+	f=open("a.txt", "w")
+	f.write("")
+	f.close()
+	print "RAW de Google:", raw
+	time.sleep(0.5)
+	n=re.compile(ur'"language":"(?P<lang>[a-z]+)","isReliable":(?P<rel>true|false),"confidence":(?P<con>[0-9\.]+)\}').finditer(raw)
+	for j in n:
+		lang=j.group("lang")
+		con=float(j.group("con"))
+		rel=j.group("rel")
+	
+	return lang, rel, con
 
 commonswiki=wikipedia.Site("commons", "commons")
 
@@ -23,54 +55,39 @@ gen=pagegenerators.AllpagesPageGenerator(start = st, namespace = 6, includeredir
 pre=pagegenerators.PreloadingGenerator(gen, pageNumber=250, lookahead=250)
 
 for page in pre:
-	#print page.title()
 	if not page.exists():
+		print "No exist"
 		continue
 	
+	wikipedia.output(u"%s\n=== %s ===" % ("#"*50, page.title()))	
+
 	wtext=page.get()
 	if not re.search(ur"(?i)\{\{ *information", wtext):
+		print "No {{Information}} template"
 		continue
 	
-	reg=ur"(?im)(?P<ini>Description *= *)(?P<desc>[^\n\r\{\/\:]+?)(?P<fin>\r\n *\| *Source *=)" # never templates
+	reg=ur"(?im)(?P<ini>Description *= *)(?P<desc>[^\n\r\{\/\:\<]+?)(?P<fin>\r\n *\| *Source *=)" # never templates, no <br>
 	m=re.compile(reg).finditer(wtext)
 	for i in m:
 		desc=i.group("desc")
-		if len(desc)<100 or len(desc)>4000: #el limite de google es 5000, probar límite inferior
+		if len(desc)<50 or len(desc)>4000: #el limite de google es 5000, probar límite inferior
 			break
-		desc_=desc
-		desc_=re.sub(ur"\[\[(?P<link>[^\]\|]+?)\]\]", ur"\g<link>", desc_)
-		desc_=re.sub(ur"\[\[(?P<link>[^\]\|]+?)\|(?P<label>[^\]\|]+?)\]\]", ur"\g<label>", desc_)
-		desc_=re.sub(ur" ", ur"%20", desc_)
 		
-		url="http://ajax.googleapis.com/ajax/services/language/detect?v=1.0&q="
-		url+=desc_.encode("utf-8")
-		wikipedia.output(u"=== %s ===" % page.title())
-		os.system("curl -e http://es.wikipedia.org/wiki/User:Emijrp '%s' > a.txt 2> /dev/null" % url)
 		
-		f=open("a.txt", "r")
-		raw=f.read()
-		#print raw
-		f.close()
 		
-		n=re.compile(ur'"language":"(?P<lang>[a-z]+)","isReliable":(?P<rel>true|false),"confidence":(?P<con>[0-9\.]+)\}').finditer(raw)
-		for j in n:
-			lang=j.group("lang")
-			con=float(j.group("con"))
-			rel=j.group("rel")
-			if rel=="true" and con>0.8:
-				wikipedia.output(u"Description: %s" % desc)
-				wikipedia.output(u"Language: %s" % lang)
-				
+		[lang, rel, con]=checkLanguage(desc)
+		if rel=="true" and con>0.5:
+			[lang1, rel1, con1]=checkLanguage(desc[:len(desc)/2])
+			[lang2, rel2, con2]=checkLanguage(desc[len(desc)/2:])
+			if lang1==lang and lang2==lang:
 				newtext=re.sub(reg, ur"\g<ini>{{%s|\g<desc>}}\g<fin>" % lang, wtext)
-				print '-'*50
-				print desc_
-				print '-'*50
-				print "Language:", lang, "	isReliable", rel, "	Confidence", con
+				print "Response by Google: Language:", lang, "	isReliable", rel, "	Confidence", con
 				print '-'*50
 				wikipedia.showDiff(wtext, newtext)
 				print '-'*50
 				page.put(newtext, u"BOT - Adding {{%s}} to description: %s" % (lang, desc))
-			break
+			else:
+				print "-----> Not conclusive <-----"
 		break
 	
 
