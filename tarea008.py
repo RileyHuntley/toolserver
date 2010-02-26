cd7777a40683236050c855af5ccb9d65af4cbbd6
emijrp <emijrp@c6dfa21e-ad1b-11dd-a35d-6bd55b4a219c>
2010-02-26 00:18:43 +0000

    cambios en ranking de ediciones (idiomas y wikt)

--- tarea008.py
+++ tarea008.py
@@ -19,60 +19,131 @@ import os, re, wikipedia, sys
 
 begin=u"''Please, translate this into your language and delete the english text'': This table shows '''first {{{1}}} users with more edits''' in this Wikipedia. Bots are not included.\n\n''If you want to change page title, contact to [[:es:User talk:Emijrp]]. Thanks.''\n\n<center>\n{| class='wikitable sortable' style='text-align:center;'\n! #\n! User\n! Edits\n"
 begin2=u"''Please, translate this into your language and delete the english text'': This table shows '''first {{{1}}} users with more edits''' in this Wikipedia. Bots are included.\n\n''If you want to change page title, contact to [[:es:User talk:Emijrp]]. Thanks.''\n\n<center>\n{| class='wikitable sortable' style='text-align:center;'\n! #\n! User\n! Edits\n"
-end=u"|}\n</center>\n\n''Please, put here a category similar to <nowiki>[[Category:Wikipedia statistics]]</nowiki>.''\n\n[[cs:Wikipedie:Nejaktivnější wikipedisté]]\n[[de:Wikipedia:Beitragszahlen]]\n[[es:Wikipedia:Lista de wikipedistas por número de ediciones]]\n[[fr:Wikipédia:Liste des Wikipédiens par nombre d'éditions]]\n[[zh-classical:維基大典:編輯次數]]\n[[id:Wikipedia:Daftar pengguna menurut jumlah suntingan]]\n[[is:Wikipedia:Notendur eftir breytingafjölda]]\n[[he:ויקיפדיה:נתונים סטטיסטיים/משתמשים/1-100]]\n[[jv:Wikipedia:Daftar panganggo miturut cacahé suntingan]]\n[[ka:ვიკიპედია:ვიკიპედიელები აქტიურობის მიხედვით]]\n[[lt:Vikipedija:Naudotojai pagal keitimų skaičių]]\n[[hu:Wikipédia:Szerkesztők listája szerkesztésszám szerint]]\n[[ja:Wikipedia:編集回数の多いウィキペディアンの一覧]]\n[[pl:Wikipedia:Najaktywniejsi wikipedyści]]\n[[pt:Wikipedia:Lista de wikipedistas por número de edições]]\n[[sq:Wikipedia:Lista e Wikipedianëve sipas redaktimeve]]\n[[uk:Вікіпедія:Найактивніші]]\n[[bat-smg:Vikipedėjė:Nauduotuojē palē keitėmu skaitliu]]\n[[zh:Wikipedia:最多贡献的用户]]"
+end=u"|}\n</center>\n\n''Please, put here a category similar to <nowiki>[[Category:Wikipedia statistics]]</nowiki>.''"
+
+"""\n\n[[cs:Wikipedie:Nejaktivnější wikipedisté]]\n[[de:Wikipedia:Beitragszahlen]]\n[[es:Wikipedia:Lista de wikipedistas por número de ediciones]]\n[[fr:Wikipédia:Liste des Wikipédiens par nombre d'éditions]]\n[[zh-classical:維基大典:編輯次數]]\n[[id:Wikipedia:Daftar pengguna menurut jumlah suntingan]]\n[[is:Wikipedia:Notendur eftir breytingafjölda]]\n[[he:ויקיפדיה:נתונים סטטיסטיים/משתמשים/1-100]]\n[[jv:Wikipedia:Daftar panganggo miturut cacahé suntingan]]\n[[ka:ვიკიპედია:ვიკიპედიელები აქტიურობის მიხედვით]]\n[[lt:Vikipedija:Naudotojai pagal keitimų skaičių]]\n[[hu:Wikipédia:Szerkesztők listája szerkesztésszám szerint]]\n[[ja:Wikipedia:編集回数の多いウィキペディアンの一覧]]\n[[pl:Wikipedia:Najaktywniejsi wikipedyści]]\n[[pt:Wikipedia:Lista de wikipedistas por número de edições]]\n[[sq:Wikipedia:Lista e Wikipedianëve sipas redaktimeve]]\n[[uk:Вікіпедія:Найактивніші]]\n[[bat-smg:Vikipedėjė:Nauduotuojē palē keitėmu skaitliu]]\n[[zh:Wikipedia:最多贡献的用户]]"
+"""
+
 end2=end
 
 tras1={
-u"ar": u"قائمة الويكيبيديين حسب عدد التعديلات",
-u"ca": u"Llista de viquipedistes per nombre d'edicions",
-u"da": u"Wikipedianere efter antal redigeringer",
-u"eo": u"Listo de uzantoj laŭ redaktonombro",
-u"es": u"Ranking de ediciones", 
-u"fa": u"فهرست کاربران ویکی‌پدیا بر اساس تعداد ویرایش‌ها",
-u"fi": u"Luettelo Wikipedian käyttäjistä muokkausmäärän mukaan",
-u"fr": u"Utilisateurs par nombre d'éditions", 
-u"gl": u"Estatísticas/Lista de usuarios por número de edicións",
-u"hr": u"Popis Wikipedista po broju uređivanja",
-u"ht": u"Lis Wikipedyen pa nonm edisyon yo fè",
-u"hu": u"Wikipédisták listája szerkesztésszám szerint",
-u"ko": u"편집횟수 순 사용자 목록",
-u"pl": u"Użytkownicy według liczby edycji",
-u"ro": u"Lista wikipediştilor după numărul de editări",
-u"simple": u"List of Wikipedians by number of changes",
-u"sl": u"Seznam Wikipedistov po številu urejanj",
-u"sv": u"Lista över Wikipedia-användare sorterad efter antalet redigeringar",
-u"th": u"รายชื่อชาววิกิพีเดียที่แก้ไขมากที่สุด 500 อันดับ",
-u"tr": u"Değişiklik sayılarına göre Vikipedistler listesi",
-u"vi": u"Danh sách thành viên Wikipedia theo số lần sửa trang",
+'wikipedia': {
+	u"ar": u"قائمة الويكيبيديين حسب عدد التعديلات",
+	u"ca": u"Llista de viquipedistes per nombre d'edicions",
+	u"da": u"Wikipedianere efter antal redigeringer",
+	u"eo": u"Listo de uzantoj laŭ redaktonombro",
+	u"es": u"Ranking de ediciones", 
+	u"fa": u"فهرست کاربران ویکی‌پدیا بر اساس تعداد ویرایش‌ها",
+	u"fi": u"Luettelo Wikipedian käyttäjistä muokkausmäärän mukaan",
+	u"fr": u"Utilisateurs par nombre d'éditions", 
+	u"gl": u"Estatísticas/Lista de usuarios por número de edicións",
+	u"hr": u"Popis Wikipedista po broju uređivanja",
+	u"ht": u"Lis Wikipedyen pa nonm edisyon yo fè",
+	u"hu": u"Wikipédisták listája szerkesztésszám szerint",
+	u"ko": u"편집횟수 순 사용자 목록",
+	u"pl": u"Użytkownicy według liczby edycji",
+	u"ro": u"Lista wikipediştilor după numărul de editări",
+	u"simple": u"List of Wikipedians by number of changes",
+	u"sl": u"Seznam Wikipedistov po številu urejanj",
+	u"sv": u"Lista över Wikipedia-användare sorterad efter antalet redigeringar",
+	u"th": u"รายชื่อชาววิกิพีเดียที่แก้ไขมากที่สุด 500 อันดับ",
+	u"tr": u"Değişiklik sayılarına göre Vikipedistler listesi",
+	u"vi": u"Danh sách thành viên Wikipedia theo số lần sửa trang",
+	},
+'wiktionary': {
+	u"es": u"Ranking de ediciones", 
+	},
+'wikinews': {
+	u"es": u"Ranking de ediciones", 
+	},
 }
+
 tras2={
-u"ar": u"قائمة الويكيبيديين حسب عدد التعديلات (متضمنة البوتات)",
-u"ca": u"Llista de viquipedistes per nombre d'edicions (bots inclosos)",
-u"da": u"Wikipedianere efter antal redigeringer (bots inkluderet)",
-u"eo": u"Listo de uzantoj laŭ redaktonombro (inkluzivante robotojn)",
-u"es": u"Ranking de ediciones (incluye bots)", 
-u"fr": u"Utilisateurs par nombre d'éditions (bots inclus)", 
-u"gl": u"Estatísticas/Lista de usuarios por número de edicións (bots incluídos)",
-u"hr": u"Popis Wikipedista po broju uređivanja (botovi uključeni)",
-u"hu": u"Wikipédisták listája szerkesztésszám szerint (botokkal)",
-u"ko": u"편집횟수 순 사용자 목록 (봇 포함)",
-u"pl": u"Użytkownicy według liczby edycji (w tym boty)",
-u"ro": u"Lista wikipediştilor după numărul de editări (inclusiv roboţi)",
-u"simple": u"List of Wikipedians by number of changes (bots included)",
-u"sl": u"Seznam Wikipedistov po številu urejanj (z boti)",
-u"sv": u"Lista över Wikipedia-användare sorterad efter antalet redigeringar (inklusive robotar)",
-u"th": u"รายชื่อชาววิกิพีเดียที่แก้ไขมากที่สุด 500 อันดับ (รวมบอต)",
-u"tr": u"Değişiklik sayılarına göre Vikipedistler listesi (botlar dahil)",
-u"vi": u"Danh sách thành viên Wikipedia theo số lần sửa trang (tính cả bot)",
+'wikipedia': {
+	u"ar": u"قائمة الويكيبيديين حسب عدد التعديلات (متضمنة البوتات)",
+	u"ca": u"Llista de viquipedistes per nombre d'edicions (bots inclosos)",
+	u"da": u"Wikipedianere efter antal redigeringer (bots inkluderet)",
+	u"eo": u"Listo de uzantoj laŭ redaktonombro (inkluzivante robotojn)",
+	u"es": u"Ranking de ediciones (incluye bots)", 
+	u"fr": u"Utilisateurs par nombre d'éditions (bots inclus)", 
+	u"gl": u"Estatísticas/Lista de usuarios por número de edicións (bots incluídos)",
+	u"hr": u"Popis Wikipedista po broju uređivanja (botovi uključeni)",
+	u"hu": u"Wikipédisták listája szerkesztésszám szerint (botokkal)",
+	u"ko": u"편집횟수 순 사용자 목록 (봇 포함)",
+	u"pl": u"Użytkownicy według liczby edycji (w tym boty)",
+	u"ro": u"Lista wikipediştilor după numărul de editări (inclusiv roboţi)",
+	u"simple": u"List of Wikipedians by number of changes (bots included)",
+	u"sl": u"Seznam Wikipedistov po številu urejanj (z boti)",
+	u"sv": u"Lista över Wikipedia-användare sorterad efter antalet redigeringar (inklusive robotar)",
+	u"th": u"รายชื่อชาววิกิพีเดียที่แก้ไขมากที่สุด 500 อันดับ (รวมบอต)",
+	u"tr": u"Değişiklik sayılarına göre Vikipedistler listesi (botlar dahil)",
+	u"vi": u"Danh sách thành viên Wikipedia theo số lần sửa trang (tính cả bot)",
+	},
+'wiktionary': {
+	u"es": u"Ranking de ediciones (incluye bots)", 
+	},
+'wikinews': {
+	u"es": u"Ranking de ediciones (incluye bots)", 
+	},
 }
 
 #do not want: nl, simple 
 # fix: hr
 projects={
-'wikinews': ['es'],
-#'wikipedia': ['es', 'eo', 'hu', 'ca', 'tr', 'ro', 'vo', 'fi', 'it', 'nl', 'ru', 'sv', 'no', 'da', 'ar', 'ko', 'sr', 'sl', 'vi', 'bg', 'et', 'ht', 'fa', 'hr', 'new', 'nn', 'te', 'gl', 'th', 'simple', 'he'],
-'wikipedia': ['es', 'hr', 'ro', 'simple', 'th', 'tr', 'vi', 'da', 'eo', 'ar'],
+	'wikinews': {
+		'es': {'rankingusers':True, 'rankingbots':True, 'limit':100},
+		},
+	'wikipedia': {
+		'es': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'hr': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'ro': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'simple': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'th': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'tr': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'vi': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'da': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'eo': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'ar': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		},
+	'wiktionary': {
+		'es': {'rankingusers':True, 'rankingbots':True, 'limit':100},
+		'simple': {'rankingusers':True, 'rankingbots':False, 'limit':100},
+		},
 }
+
+"""'wikipedia': {
+		'es': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'eo': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'hu': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'ca': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'tr': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'ro': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'vo': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'fi': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'it': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'nl': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'ru': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'sv': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'no': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'da': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'ar': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'ko': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'sr': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'sl': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'vi': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'bg': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'et': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'ht': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'fa': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'hr': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'new': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'nn': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'te': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'gl': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'th': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'simple': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		'he': {'rankingusers':True, 'rankingbots':True, 'limit':500},
+		},"""
 
 #if len(sys.argv)>1:
 #	lll=[sys.argv[1]]
@@ -83,7 +154,7 @@ iws2={}
 for family, langs in projects.items():
 	iws1[family]=[]
 	iws2[family]=[]
-	for lang in langs:
+	for lang, v in langs.items():
 		site=wikipedia.Site(lang, family)
 		data=site.getUrl("/w/index.php?title=Special:RecentChanges&limit=0")
 		data=data.split('<select id="namespace" name="namespace" class="namespaceselector">')[1].split('</select>')[0]
@@ -95,12 +166,12 @@ for family, langs in projects.items():
 			if number=='4':
 				wikipedianm+=name
 		traslation1="List of Wikipedians by number of edits"
-		if tras1.has_key(lang):
-			traslation1=tras1[lang]
-		iws1[family].append(u"[[%s:%s:%s]]" % (lang, wikipedianm, traslation1)
+		if tras1[family].has_key(lang):
+			traslation1=tras1[family][lang]
+		iws1[family].append(u"[[%s:%s:%s]]" % (lang, wikipedianm, traslation1))
 		traslation2="List of Wikipedians by number of edits (bots included)"
-		if tras2.has_key(lang):
-			traslation2=tras2[lang]
+		if tras2[family].has_key(lang):
+			traslation2=tras2[family][lang]
 		iws2[family].append(u"[[%s:%s:%s]]" % (lang, wikipedianm, traslation2))
 	iws1[family].sort()
 	iws2[family].sort()
@@ -108,7 +179,7 @@ for family, langs in projects.items():
 for family, langs in projects.items():
 	for lang in langs:
 		#la lista de bots debe ir dentro del bucle
-		bots=[u'BOTpolicia', u'AVBOT', u'CommonsDelinker', u'Eskimbot', u'EmxBot', u'YurikBot', u'H-Bot', u'Paulatz bot', u'TekBot', u'Alfiobot', u'RoboRex', u'Agtbot', u'Felixbot', u'Pixibot', u'Sz-iwbot', u'Timbot (Gutza)', u'Ginosbot', u'GrinBot', u'.anacondabot', u'Omdirigeringsrättaren', u'Rubinbot', u'HasharBot', u'NetBot', u"D'ohBot", u'Byrialbot', u'Broadbot', u'Guanabot', u'Chris G Bot 2', u'CCyeZBot', u'Soulbot', u'MSBOT', u'GnawnBot', u'Chris G Bot 3', u'Huzzlet the bot', u'JCbot', u'DodekBot', u'John Bot II', u'CyeZBot', u'Beefbot', u'Louperibot', u'SOTNBot', u'DirlBot', u'Obersachsebot', u'WikiDreamer Bot', u'YonaBot', u'Chlewbot', u'PixelBot', u'ToePeu.bot', u'HujiBot', u'Le Pied-bot', u'Ugur Basak Bot', u'NigelJBot', u'CommonsTicker', u'Tangobot', u'SeanBot', u'Corrector de redirecciones', u'HermesBot', u'Darkicebot', u'RedBot', u'HerculeBot', u'PatruBOT']
+		bots=[u'BOTpolicia', u'AVBOT', u'CommonsDelinker', u'Eskimbot', u'EmxBot', u'YurikBot', u'H-Bot', u'Paulatz bot', u'TekBot', u'Alfiobot', u'RoboRex', u'Agtbot', u'Felixbot', u'Pixibot', u'Sz-iwbot', u'Timbot (Gutza)', u'Ginosbot', u'GrinBot', u'.anacondabot', u'Omdirigeringsrättaren', u'Rubinbot', u'HasharBot', u'NetBot', u"D'ohBot", u'Byrialbot', u'Broadbot', u'Guanabot', u'Chris G Bot 2', u'CCyeZBot', u'Soulbot', u'MSBOT', u'GnawnBot', u'Chris G Bot 3', u'Huzzlet the bot', u'JCbot', u'DodekBot', u'John Bot II', u'CyeZBot', u'Beefbot', u'Louperibot', u'SOTNBot', u'DirlBot', u'Obersachsebot', u'WikiDreamer Bot', u'YonaBot', u'Chlewbot', u'PixelBot', u'ToePeu.bot', u'HujiBot', u'Le Pied-bot', u'Ugur Basak Bot', u'NigelJBot', u'CommonsTicker', u'Tangobot', u'SeanBot', u'Corrector de redirecciones', u'HermesBot', u'Darkicebot', u'RedBot', u'HerculeBot', u'PatruBOT', u'RobotGMwikt', u'MonoBot', u'WikimediaNotifier', u'', u'', u'', u'', u'', u'', u'', u'',]
 		site=wikipedia.Site(lang, family)
 		
 		data=site.getUrl("/w/index.php?title=Special:Listusers&limit=5000&group=bot")
@@ -139,6 +210,8 @@ for family, langs in projects.items():
 		family2='wiki'
 		if family=='wikinews':
 			family2='wikinews'
+		elif family=='wiktionary':
+			family2='wiktionary'
 		
 		os.system('mysql -h %s%s-p.db.toolserver.org -e "use %s%s_p;select user_name, user_editcount from user where user_editcount!=0 order by user_editcount desc limit 5000;" > /home/emijrp/temporal/tarea008.data' % (lang, family2, lang, family2))
 		
@@ -150,7 +223,7 @@ for family, langs in projects.items():
 		c=1
 		cbots=1
 		cplanti2=1
-		cuantos=500
+		cuantos=projects[family][lang]['limit']
 		s=u"{{/begin|%d}}\n" % cuantos
 		sbots=u"{{/begin|%d}}\n" % cuantos
 		planti2=u"{{#switch:{{{1|User}}}\n"
@@ -196,39 +269,39 @@ for family, langs in projects.items():
 		#first ranking
 		if len(s)>1000: #evitando errores de db replication
 			title=u''
-			if tras1.has_key(lang):
-				title=u"%s:%s" % (wikipedianm, tras1[lang])
+			if tras1[family].has_key(lang):
+				title=u"%s:%s" % (wikipedianm, tras1[family][lang])
 			else:
 				title=u"%s:List of Wikipedians by number of edits" % wikipedianm
 			page=wikipedia.Page(site, title)
-			if not page.isRedirectPage() and not page.isDisambig():
+			if projects[family][lang]['rankingusers'] and not page.isRedirectPage() and not page.isDisambig():
 				page.put(s, resume)
 		
 		#begin & end
 		page=wikipedia.Page(site, u"%s/begin" % title)
-		if not page.exists():
+		if projects[family][lang]['rankingusers'] and not page.exists():
 			page.put(begin, resume)
 		page=wikipedia.Page(site, u"%s/end" % title)
-		if not page.exists():
+		if projects[family][lang]['rankingusers'] and not page.exists():
 			page.put(end, resume)
 		
 		#second ranking
-		if len(sbots)>1000: #evitando errores de db replication
+		if len(sbots)>1000 and (family!='wiktionary' and lang!='simple'): #evitando errores de db replication
 			title=u''
-			if tras2.has_key(lang):
-				title=u"%s:%s" % (wikipedianm, tras2[lang])
+			if tras2[family].has_key(lang):
+				title=u"%s:%s" % (wikipedianm, tras2[family][lang])
 			else:
 				title=u"%s:List of Wikipedians by number of edits (bots included)" % wikipedianm
 			page=wikipedia.Page(site, title)
-			if not page.isRedirectPage() and not page.isDisambig():
+			if projects[family][lang]['rankingbots'] and not page.isRedirectPage() and not page.isDisambig():
 				page.put(sbots, resume)
 		
 		#begin & end
 		page=wikipedia.Page(site, u"%s/begin" % title)
-		if not page.exists():
+		if projects[family][lang]['rankingbots'] and not page.exists():
 			page.put(begin2, resume)
 		page=wikipedia.Page(site, u"%s/end" % title)
-		if not page.exists():
+		if projects[family][lang]['rankingbots'] and not page.exists():
 			page.put(end2, resume)
 		
 		#otras plantillas
