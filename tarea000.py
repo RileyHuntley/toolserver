996d629f87983f7de980edd356ab76f8fa0563a6
emijrp <emijrp@c6dfa21e-ad1b-11dd-a35d-6bd55b4a219c>
2010-04-02 23:18:35 +0000

    exclusiones

--- tarea000.py
+++ tarea000.py
@@ -155,8 +155,9 @@ def getServer(lang, family):
 def isExcluded(tarea, family, lang):
 	excludedprojects={
 		'tarea008': {
-			'wikipedia': ['ko', # http://es.wikipedia.org/w/index.php?title=Usuario_Discusi%C3%B3n:Emijrp&diff=35727924&oldid=35727631
-				'pl', #http://es.wikipedia.org/w/index.php?title=Usuario_Discusi%C3%B3n:Emijrp&diff=35726268&oldid=35726116
+			'wikipedia': ['ko', #no les interesa http://es.wikipedia.org/w/index.php?title=Usuario_Discusi%C3%B3n:Emijrp&diff=35727924&oldid=35727631
+				'pl', #ya tienen uno http://es.wikipedia.org/w/index.php?title=Usuario_Discusi%C3%B3n:Emijrp&diff=35726268&oldid=35726116
+				'hu', #ya tienen su propio ranking http://hu.wikipedia.org/wiki/Wikip%C3%A9dia:Wikip%C3%A9dist%C3%A1k_list%C3%A1ja_szerkeszt%C3%A9ssz%C3%A1m_szerint lo desactivo porque les recreaba las páginas /begin y /end
 				],
 			'wiktionary': [],
 			'wikinews': [],
