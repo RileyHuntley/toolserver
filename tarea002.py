7c0ad16c51c7e33671e2c05f5fbbfc0d4aafdf2f
emijrp <emijrp@c6dfa21e-ad1b-11dd-a35d-6bd55b4a219c>
2010-04-24 21:09:33 +0000

    pylint 10

--- tarea002.py
+++ tarea002.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2009 emijrp
@@ -14,53 +15,129 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+""" Update a words progress template """
+
 from __future__ import generators
-import sys, re
-import wikipedia, pagegenerators,catlib, config,time, thread
+import re
 import urllib
 
+import wikipedia
 
-pre=u"Usuario:Emijrp/MiPortal/Lemario/"
+def main():
+    """ Update a words progress template """
+    
+    wikies = wikipedia.Site("es", "wikipedia")
+    prefix = "Usuario:Emijrp/MiPortal/Lemario/"
+    suffixes = ["a-absolver", "absorbencia-achicadura", 
+                "achicamiento-acullicar", "acullico-aeromotor", 
+                "aeromoza-agravación", "agravador-ajimezado", 
+                "ajipa-alcantarillar", "alcantarillero-alhorra", 
+                "alhorre-aloja", "alojado-ambientar", 
+                "ambiente-anatomopatológico", 
+                "anatomopatólogo-antecapilla", 
+                "antecedencia-apaleador", "apaleamiento-apresto", 
+                "apresura-argelino", "argemone-arrinconar", 
+                "arriostrar-asistimiento", "asistir-atisuado", 
+                "atizacandiles-avance", "avandicho-ábsida", 
+                "ábside-balotaje", "balotar-basilisco", 
+                "basis-besalamano", "besamanos-blandón", 
+                "blanqueación-borneadizo", "borneadura-broquelazo",
+                "broquelero-cabalgar", "cabalgata-cajo", 
+                "cajonada-camachuelo", "camada-canijo", 
+                "canil-carapachay", "carapacho-carraña", 
+                "carrañaca-catascopio", "catasta-cedrito", 
+                "cedro-ceroferario", "ceroleína-chape", 
+                "chapeado-chipojo", "chipolo-churumen", "churumo-cirio",
+                "cirolero-cochama", "cochambre-colofonita", 
+                "colofonía-compuerta", 
+                "compuestamente-congelativo", "congenial-continuidad", 
+                "continuismo-copiar", "copichuela-corruco", 
+                "corrugación-criadero", "criadilla-cuatrín", 
+                "cuatí-curtimiento", "curtir-decadente", 
+                "decadentismo-demográfico", "demoledor-desairar", 
+                "desaire-descargamiento", "descargar-desenastar", 
+                "desencabalgado-desimponer", "desimpresionar-despego", 
+                "despegue-desvergonzado", "desvergonzarse-dignificar", 
+                "digno-disyuntiva", "disyuntivamente-dubitativo", 
+                "ducado-ejemplo", "ejercer-embrisar", 
+                "embriólogo-enardecedor", "enardecer-encostradura",
+                "encostrar-engranerar", "engranujarse-enseño", 
+                "enseñoramiento-envejecido", 
+                "envejecimiento-escalfecerse", "escalfeta-escuadreo", 
+                "escuadrilla-esperteza", "esperón-estelado", 
+                "estelar-estúpidamente", 
+                "estúpido-expedienteo", "expedir-falciforme", 
+                "falcinelo-ferial", "feriante-fitográfico", 
+                "fitolacáceo-fornicar", "fornicario-frotador", 
+                "frotadura-galancete", "galanga-garrobal", 
+                "garrobilla-ginefobia", "ginesta-granadí", 
+                "granalla-guaquero", "guara-güimba", 
+                "güin-hemograma", "hemolisina-hilandería", 
+                "hilanza-hontanal", "hontanar-húmico", 
+                "húmido-impresionar", "impresionismo-incumplir", 
+                "incunable-infrasonido", 
+                "infrautilización-institucionalización", 
+                "institucionalizar-inventación", 
+                "inventador-jactancioso", "jactar-jubetería", 
+                "jubilación-lagrimeo", "lagrimoso-lebulense", 
+                "lecanomancia-lidio", "lidioso-loanda", 
+                "loanza-lúcidamente", "lúcido-malaleche", 
+                "malambo-manifestador", "manifestante-marqueta", 
+                "marquetería-mecedor", "mecedura-mercadero", 
+                "mercadería-miligramo", "mililitro-moharrache", 
+                "moharracho-morcillo", "morcillón-mundanalidad", 
+                "mundanamente-Leiden", "Leo-nefrosis", 
+                "nefrítico-nonada", "nonagenario-obsidional", 
+                "obsolescencia-onerario", "oneroso-ostrogodo", 
+                "ostrícola-pallar", "pallas-paraliticado", 
+                "paraliticarse-pastoría", "pastosidad-peindra", 
+                "peine-perforación", "perforador-petulantemente", 
+                "petunia-pira", "pirado-pleon", "pleonasmo-pontonero", 
+                "pontífice-preceder", "precelente-primate", 
+                "primavera-prosperidad", "prostaféresis-punterola",
+                "puntería-quimerino", "quimerista-ramoneo", 
+                "ramoso-rebuscar", "rebusco-redova", 
+                "redrar-rejuvenecimiento", "rejín-repodrir", 
+                "repollar-retardativo", "retardatorio-ringlero", 
+                "ringorrango-rotario", "rotativo-sacro", 
+                "sacrosantamente-sangradera", "sangrado-sedeño", 
+                "sedicente-serretazo", "serrezuela-sincronización",
+                "sincronizar-sociometría", 
+                "sociométrico-sosiega", "sosiego-sulfurado", 
+                "sulfurar-tablado", "tablaje-taraceador", 
+                "taracear-tempestear", "tempestivamente-tesón", 
+                "teta-tochedad", "tochibí-toxicogénesis", 
+                "toxicología-trasfollado", "trasfollo-trimensual", 
+                "trimestral-tuna", "tunal-unívocamente", 
+                "unívoco-varioloso", "variopinto-vergelero", 
+                "vergeta-virreino", "virrey-xi", "xifoideo-zapoyolito", 
+                "zapuyul-"]
+    output = u"{{subst:ProgresoLemario/subst"
+    totalred = 0
+    totalblue = 0
+     
+    for suffix in suffixes:
+        wikipedia.output("Analizando [%s]" % suffix)
+        
+        url = "/w/index.php?title=%s%s" % (prefix, urllib.quote(suffix))
+        raw = wikies.getUrl(url)        
+        raw = raw.split("start content")[1].split("end content")[0]
+        
+        red = len(re.findall('class="new"', raw))
+        blue = len(re.findall('/wiki/', raw))
+        totalred += red
+        totalblue += blue
+        percent = blue * 1.0 / (red + blue) * 100
+        
+        wikipedia.output("%d redlinks, %d blue links, %.2f percent \
+                          (red links)" % (red, blue, percent))
+        output += u"|%.2f" % (percent)
 
-salida=u"{{subst:ProgresoLemario/subst"
+    percent = totalblue * 1.0 / (totalred + totalblue) * 100
+    output += u"|%.2f" % (percent)
+    output += u"}}<noinclude>{{documentación de plantilla}}</noinclude>"
+    page = wikipedia.Page(wikies, "Plantilla:ProgresoLemario")
+    page.put(output, "BOT - Actualizando plantilla %.2f%%" % (percent))
 
-tr=0
-ta=0
-
-for p in [u"a-absolver", u"absorbencia-achicadura", u"achicamiento-acullicar", u"acullico-aeromotor", u"aeromoza-agravaci%C3%B3n", u"agravador-ajimezado", u"ajipa-alcantarillar", u"alcantarillero-alhorra", u"alhorre-aloja", u"alojado-ambientar", u"ambiente-anatomopatol%C3%B3gico", u"anatomopat%C3%B3logo-antecapilla", u"antecedencia-apaleador", u"apaleamiento-apresto", u"apresura-argelino", u"argemone-arrinconar", u"arriostrar-asistimiento", u"asistir-atisuado", u"atizacandiles-avance", u"avandicho-%C3%A1bsida", u"%C3%A1bside-balotaje", u"balotar-basilisco", u"basis-besalamano", u"besamanos-bland%C3%B3n", u"blanqueaci%C3%B3n-borneadizo", u"borneadura-broquelazo", u"broquelero-cabalgar", u"cabalgata-cajo", u"cajonada-camachuelo", u"camada-canijo", u"canil-carapachay", u"carapacho-carra%C3%B1a", u"carra%C3%B1aca-catascopio", u"catasta-cedrito", u"cedro-ceroferario", u"cerole%C3%ADna-chape", u"chapeado-chipojo", u"chipolo-churumen", u"churumo-cirio", u"cirolero-cochama", u"cochambre-colofonita", u"colofon%C3%ADa-compuerta", u"compuestamente-congelativo", u"congenial-continuidad", u"continuismo-copiar", u"copichuela-corruco", u"corrugaci%C3%B3n-criadero", u"criadilla-cuatr%C3%ADn", u"cuat%C3%AD-curtimiento", u"curtir-decadente", u"decadentismo-demogr%C3%A1fico", u"demoledor-desairar", u"desaire-descargamiento", u"descargar-desenastar", u"desencabalgado-desimponer", u"desimpresionar-despego", u"despegue-desvergonzado", u"desvergonzarse-dignificar", u"digno-disyuntiva", u"disyuntivamente-dubitativo", u"ducado-ejemplo", u"ejercer-embrisar", u"embri%C3%B3logo-enardecedor", u"enardecer-encostradura", u"encostrar-engranerar", u"engranujarse-ense%C3%B1o", u"ense%C3%B1oramiento-envejecido", u"envejecimiento-escalfecerse", u"escalfeta-escuadreo", u"escuadrilla-esperteza", u"esper%C3%B3n-estelado", u"estelar-est%C3%BApidamente", u"est%C3%BApido-expedienteo", u"expedir-falciforme", u"falcinelo-ferial", u"feriante-fitogr%C3%A1fico", u"fitolac%C3%A1ceo-fornicar", u"fornicario-frotador", u"frotadura-galancete", u"galanga-garrobal", u"garrobilla-ginefobia", u"ginesta-granad%C3%AD", u"granalla-guaquero", u"guara-g%C3%BCimba", u"g%C3%BCin-hemograma", u"hemolisina-hilander%C3%ADa", u"hilanza-hontanal", u"hontanar-h%C3%BAmico", u"h%C3%BAmido-impresionar", u"impresionismo-incumplir", u"incunable-infrasonido", u"infrautilizaci%C3%B3n-institucionalizaci%C3%B3n", u"institucionalizar-inventaci%C3%B3n", u"inventador-jactancioso", u"jactar-jubeter%C3%ADa", u"jubilaci%C3%B3n-lagrimeo", u"lagrimoso-lebulense", u"lecanomancia-lidio", u"lidioso-loanda", u"loanza-l%C3%BAcidamente", u"l%C3%BAcido-malaleche", u"malambo-manifestador", u"manifestante-marqueta", u"marqueter%C3%ADa-mecedor", u"mecedura-mercadero", u"mercader%C3%ADa-miligramo", u"mililitro-moharrache", u"moharracho-morcillo", u"morcill%C3%B3n-mundanalidad", u"mundanamente-Leiden", u"Leo-nefrosis", u"nefr%C3%ADtico-nonada", u"nonagenario-obsidional", u"obsolescencia-onerario", u"oneroso-ostrogodo", u"ostr%C3%ADcola-pallar", u"pallas-paraliticado", u"paraliticarse-pastor%C3%ADa", u"pastosidad-peindra", u"peine-perforaci%C3%B3n", u"perforador-petulantemente", u"petunia-pira", u"pirado-pleon", u"pleonasmo-pontonero", u"pont%C3%ADfice-preceder", u"precelente-primate", u"primavera-prosperidad", u"prostaf%C3%A9resis-punterola", u"punter%C3%ADa-quimerino", u"quimerista-ramoneo", u"ramoso-rebuscar", u"rebusco-redova", u"redrar-rejuvenecimiento", u"rej%C3%ADn-repodrir", u"repollar-retardativo", u"retardatorio-ringlero", u"ringorrango-rotario", u"rotativo-sacro", u"sacrosantamente-sangradera", u"sangrado-sede%C3%B1o", u"sedicente-serretazo", u"serrezuela-sincronizaci%C3%B3n", u"sincronizar-sociometr%C3%ADa", u"sociom%C3%A9trico-sosiega", u"sosiego-sulfurado", u"sulfurar-tablado", u"tablaje-taraceador", u"taracear-tempestear", u"tempestivamente-tes%C3%B3n", u"teta-tochedad", u"tochib%C3%AD-toxicog%C3%A9nesis", u"toxicolog%C3%ADa-trasfollado", u"trasfollo-trimensual", u"trimestral-tuna", u"tunal-un%C3%ADvocamente", u"un%C3%ADvoco-varioloso", u"variopinto-vergelero", u"vergeta-virreino", u"virrey-xi", u"xifoideo-zapoyolito", u"zapuyul-"]:
-        wikipedia.output(u"Analizando [%s]" % p)
-        
-        #page=wikipedia.Page(wikipedia.Site("es", "wikipedia"), u"%s" % p)
-        
-        s=wikipedia.Site("es", "wikipedia")
-        url=u"/w/index.php?title=%s%s" % (pre, p)
-        d=s.getUrl(url)
-        
-        trozos=d.split("start content")
-        trozos=trozos[1].split("end content")
-        c=trozos[0]
-        
-        m=re.compile("class=\"new\"").finditer(c)
-        n=re.compile("\"/wiki/").finditer(c)
-        rojos=0
-        azules=0
-        
-        for i in m:
-                rojos+=1
-        tr+=rojos
-        
-        for i in n:
-                azules+=1
-        ta+=azules
-        
-        por=azules*1.0/(rojos+azules)*100
-        
-        wikipedia.output(u"rojos=%d azules=%d por=%.2f" % (rojos, azules, por))
-        
-        salida+=u"|%.2f" % (por)
-
-por=ta*1.0/(tr+ta)*100
-salida+=u"|%.2f" % (por)
-salida+=u"}}<noinclude>{{documentación de plantilla}}</noinclude>"
-wi=wikipedia.Page(wikipedia.Site("es", "wikipedia"), u"Plantilla:ProgresoLemario")
-wi.put(salida, u"BOT - Actualizando plantilla %.2f%s" % (por, "%"))
+if __name__ == "__main__":
+    main()
