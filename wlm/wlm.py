05366492c296a277c04f5338d3813bf7f810fd5b
emijrp <emijrp@c6dfa21e-ad1b-11dd-a35d-6bd55b4a219c>
2011-08-06 20:39:58 +0000

    WLM, mapas por provincias

--- wlm/wlm.py
+++ wlm/wlm.py
@@ -168,7 +168,7 @@ regexp_gl = re.compile(ur'(?im)\{\{\s*BIC\s*\|\s*nomeoficial\s*=\s*(?P<nombre>[^
  | imatge = 
 }}
 """
-regexp_ca = re.compile(ur'(?im)\{\{\s*filera BIC(IN)?\s*\|\s*nom\s*=\s*(?P<nombre>[^=}]*)\s*\|\s*nomcoor\s*=\s*(?P<nombrecoor>[^=}]*)\s*(\|\s*tipus\s*=\s*(?P<tipobic>[^=}]*)\s*)?(\|\s*estil\s*=\s*([^=}]*)\s*)?(\|\s*època\s*=\s*([^=}]*)\s*)?\|\s*municipi\s*=\s*(?P<municipio>[^=}]*)\s*\|\s*lloc\s*=(?P<lugar>[^=}]*)\s*\|\s*lat\s*=\s*(?P<lat>[^=}]*)\s*\|\s*lon\s*=\s*(?P<lon>[^=}]*)\s*(\|\s*idurl\s*=\s*([^=}]*)\s*)?(\|\s*prot\s*=\s*([^=}]*)\s*)?(\|\s*bcin\s*=\s*([^=}]*)\s*)?\|\s*bic\s*=\s*(?P<bic>[^=}]*)\s*(\|\s*fecha\s*=\s*(?P<fecha>[^=}]*)\s*)?\|\s*imatge\s*=\s*(?P<imagen>[^=}]*)\s*\}\}')
+regexp_ca = re.compile(ur'(?im)\{\{\s*filera (BIC|BICIN|BIC Val)\s*\|\s*nom\s*=\s*(?P<nombre>[^=}]*)\s*\|\s*nomcoor\s*=\s*(?P<nombrecoor>[^=}]*)\s*(\|\s*tipus\s*=\s*(?P<tipobic>[^=}]*)\s*)?(\|\s*estil\s*=\s*([^=}]*)\s*)?(\|\s*època\s*=\s*([^=}]*)\s*)?\|\s*municipi\s*=\s*(?P<municipio>[^=}]*)\s*\|\s*lloc\s*=(?P<lugar>[^=}]*)\s*\|\s*lat\s*=\s*(?P<lat>[^=}]*)\s*\|\s*lon\s*=\s*(?P<lon>[^=}]*)\s*(\|\s*idurl\s*=\s*([^=}]*)\s*)?(\|\s*prot\s*=\s*([^=}]*)\s*)?(\|\s*bcin\s*=\s*([^=}]*)\s*)?\|\s*bic\s*=\s*(?P<bic>[^=}]*)\s*(\|\s*fecha\s*=\s*(?P<fecha>[^=}]*)\s*)?\|\s*imatge\s*=\s*(?P<imagen>[^=}]*)\s*\}\}')
 
 
 missingcoordinates = 0
@@ -309,7 +309,7 @@ if (isset($_GET['place']))
 
 <body>
 <center>
-<table width=1024px style="text-align: center;">
+<table width=1000px style="text-align: center;">
 <tr>
 <td>
 <a href="http://www.wikimedia.org.es/" target="_blank"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Wikimedia-es-logo.svg/100px-Wikimedia-es-logo.svg.png" /></a>
