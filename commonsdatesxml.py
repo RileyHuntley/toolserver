12b35d438b19c1260435d6f529580376ebf022ed
emijrp <emijrp@c6dfa21e-ad1b-11dd-a35d-6bd55b4a219c>
2011-11-02 14:42:26 +0000

    commons dates using xml dump;

--- commonsdatesxml.py
+++ commonsdatesxml.py
@@ -42,37 +42,72 @@ def main():
         u"december":u"12", u"dec":u"12",
         
         #Spanish
+        u"enero":u"01", u"ene":u"01", 
+        u"febrero":u"02", 
+        u"marzo":u"03",
+        u"abril":u"04", u"abr":u"04", 
+        u"mayo":u"05",
+        u"junio":u"06", 
+        u"julio":u"07", 
+        u"agosto":u"08", u"ago":u"08", u"agos":u"08", 
+        u"setiembre":u"09", u"septiembre":u"09",
+        u"octubre":u"10",
+        u"noviembre":u"11",
+        u"diciembre":u"12", u"dic":u"12",
+        
+        #french
+        
+        #german
     }
     
     dumppath = ''
-    if len(sys.argv) == 2:
+    dumpfilename = ''
+    mode = ''
+    skip = ''#'File:Lagothrix lagotricha.jpg'
+    if len(sys.argv) >= 2:
         dumpfilename = sys.argv[1]
-    if len(sys.argv) == 3: #en1, fr1, etc, regexps
+    if len(sys.argv) >= 3: #en1, fr1, etc, regexps
         mode = sys.argv[2]
-    else:
+    if not mode:
         mode = 'en1'
+    if len(sys.argv) >= 4:
+        skip = sys.argv[3]
     
     xml = xmlreader.XmlDump('%s%s' % (dumppath and '%s/' % dumppath or '', dumpfilename), allrevisions=False)
     c = 0
     
+    if skip:
+        print 'Skiping to...', skip
     for x in xml.parse(): #parsing the whole dump
         if not x.title.strip().startswith('File:'):
             continue
         c += 1
-        #print x.title
+        if skip:
+            if x.title.strip() != skip:
+                continue
+            else:
+                skip = ''
         
         #regexps
         regexp_r = {
             'en1': ur"(?im)^(?P<all>(?P<ini>\s*\|\s*Date\s*=\s*)(?P<date>(?P<day>[1-9]|1[0-9]|2[0-9]|3[0-1])\s*(?P<month>January|Jan|February|Feb|March|Mar|April|Apr|May|June|Jun|July|Jul|August|Aug|September|Sept?|October|Oct|November|Nov|December|Dec)\s*(?P<year>\d{4}))(?P<end>\s*))$",
+            'es1': ur"(?im)^(?P<all>(?P<ini>\s*\|\s*Date\s*=\s*)(?P<date>(?P<day>[1-9]|1[0-9]|2[0-9]|3[0-1])\s+de\s+(?P<month>Enero|Ene|Febrero|Feb|Marzo|Mar|Abril|Abr|Mayo|May|Junio|Jun|Julio|Jul|Agosto|Ago|Septiembre|Sept?|Octubre|Oct|Noviembre|Nov|Diciembre|Dic)\s+de\s+(?P<year>\d{4}))(?P<end>\s*))$",
             
         }
         
         m = re.findall(regexp_r[mode], x.text) # check dump text
         if m:
-            print 'DUMP SAYS: ', x.title
+            print c, 'DUMP SAYS: ', x.title
+            
             page = wikipedia.Page(wikipedia.Site("commons", "commons"), x.title)
+            if not page.exists() or page.isRedirectPage() or page.isDisambig():
+                continue #next page in dump
+            if not page.canBeEdited():
+                continue
+            
             wtext = page.get()
             newtext = wtext
+            
             m = re.finditer(regexp_r[mode], wtext) # check live text
             for i in m:
                 print '   LIVE TEXT NEED TRANSLATION:', x.title
@@ -80,11 +115,13 @@ def main():
                 #replacement
                 regexp_rep = {
                     'en1': i.group('all'),
+                    'es1': i.group('all'),
                     
                 }
                 #sub
                 regexp_sub = { 
                     'en1': ur"%s%s-%s-%02d%s" % (i.group('ini'), i.group('year'), month2number[i.group('month').strip().lower()], int(i.group('day')), i.group('end')),
+                    'es1': ur"%s%s-%s-%02d%s" % (i.group('ini'), i.group('year'), month2number[i.group('month').strip().lower()], int(i.group('day')), i.group('end')),
                     
                 }
                 
