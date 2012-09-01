eebbf9b505a616e48c3d70abbf97c7ebbdb1c1eb
emijrp <emijrp@c6dfa21e-ad1b-11dd-a35d-6bd55b4a219c>
2012-09-01 21:40:22 +0000

    new version of WLM map generator

--- wlm/wlm2.py
+++ wlm/wlm2.py
@@ -272,7 +272,7 @@ def main():
         #country html
         countriessort = countrynames.keys()
         countriessort.sort()
-        moremaps = u' - '.join([u'<a href="../%s">%s</a>' % (cc, countrynames[cc]) for cc in countriessort])
+        moremaps = u' - '.join([u'<a href="../%s">%s</a>' % (re.sub(' ', '', countrynames[cc].lower()), countrynames[cc]) for cc in countriessort])
         output = u"""<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
         <html lang="en" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
         <head>
